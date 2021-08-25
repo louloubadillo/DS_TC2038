c3c28109e137f026bc634800ff8e13ec9b3b79b9
louloubadillo <lulu.badillo.csm@gmail.com>
2021-08-25 15:00:59 -0500

    finish scheduling algorithm

--- Paradigms/Greedy/scheduling.cpp
+++ Paradigms/Greedy/scheduling.cpp
@@ -6,24 +6,88 @@ using namespace std;
 
 class Task{
     public:   
+        int index; 
         int start; //starting time
         int end; //finish time
         Task(){}
-        Task(int start, int end){
+        Task(int index, int start, int end){
+            this->index = index;
             this->start = start;
             this->end = end;
         }
         void print(){
             cout<<this->start<<"->"<<this->end<<endl;
-        }
-        
+        }   
+        friend ostream& operator<<(ostream& os, const Task& selected){
+            os << "(" << selected.index << ") " << selected.start<<"->"<<selected.end;
+            return os;
+        } 
 }; 
 
- sortTasks(Task allTasks[]){
-    // Sort by earliest finishing time
-    
+//Compare tasks 
+bool areCompatible(Task t1, Task t2){
+    // If they dont overlap
+    return t1.start >= t2.end || t2.start >= t1.end;
 }
 
+//Sort by earliest finishing time
+template<class T>
+void swap(T* a, T* b) { 
+    //Swap Tasks
+    T selected = *a; 
+    *a = *b; 
+    *b = selected; 
+} 
+
+int partition (Task tasks[], int low, int high) { 
+    Task pivot = tasks[high]; 
+    int i = (low - 1); 
+    for (int j = low; j <= high - 1; j++) { 
+        if (tasks[j].end < pivot.end) { //Compares end time, sorting by earliest end time 
+            i++;
+            swap(&tasks[i], &tasks[j]); //Swaps Tasks
+        } 
+    } 
+    swap(&tasks[i + 1], &tasks[high]); 
+    return (i + 1); 
+} 
+
+void quickSort(Task tasks[], int low, int high) { 
+    if (low < high) 
+    { 
+        int indx = partition(tasks, low, high); 
+        quickSort(tasks, low, indx - 1); 
+        quickSort(tasks, indx + 1, high); 
+    } 
+} 
+
+// Print array of Tasks
+void printArray(Task arr[], int size) { 
+    for (int i = 0; i < size; i++) cout << arr[i] << " "; 
+    cout << endl; 
+} 
+
+vector<int> findCompatible(Task allTasks[], int N){
+
+    //Sort by earliest end time
+    quickSort(allTasks, 0, N-1); 
+    //Vector with the indexes of compatible tasks
+    vector<int> result; 
+    // Current task
+    Task selected = allTasks[0];
+    // 
+    result.push_back(selected.index);
+    // Iterate all tasks
+    for(int i = 0; i<N; i++){
+        if( areCompatible(selected, allTasks[i]) ){
+            result.push_back(allTasks[i].index);
+            selected = allTasks[i]; 
+        }
+    }
+    // Order results 
+    sort(result.begin(), result.end());
+    return result; 
+}
 
 /**
  *
@@ -39,8 +103,7 @@ int main(int argc, char** argv){
     // 2 - N+2 Start values
     int startValues[N]; 
     for(int i = 0; i<N; i++){
-        startValues[i] = atoi(argv[i+2]);
-        
+        startValues[i] = atoi(argv[i+2]);   
     }
     // N - 2*N End values
     int endValues[N]; 
@@ -50,16 +113,15 @@ int main(int argc, char** argv){
     // Create tasks
     Task allTasks[N];
     for(int i = 0; i<N; i++){
-        if(startValues[i] < endValues[i]){
-            Task t(startValues[i], endValues[i]);
-            //Testing
-            allTasks[i] = t;
-            t.print();
-        }
-        else{
-            cout << "The start time can't be larget than the finish time" << endl; 
-        }   
-    } 
+        Task selected(i, startValues[i], endValues[i]);
+        allTasks[i] = selected;
+    }
+    // Resultados
+    vector<int> resultados = findCompatible(allTasks, N);
 
+    for(int i = 0; i < resultados.size(); i++){
+        cout << resultados[i] << " ";  
+    } 
+    //imprimir result printArray(allTasks, N);
     return 0;
 }
